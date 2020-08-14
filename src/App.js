1788fa6c94ee43a455e664e91db78af23792890c
yichi <yichi.zhang@thoughtworks.com>
2020-08-14 12:03:09 +0800

    [zycsustc]-refactor page styl

--- src/App.js
+++ src/App.js
@@ -11,13 +11,13 @@ class App extends Component {
 
     componentWillMount() {
         this.setState({
-            result: "result",
+            result: "No result yet",
             nodes: ["A", "B", "C", "D", "E"],
         })
     }
 
     onClickExactPath = async () => {
-        const url = this.getPaths("exactPath")+this.state.exactPath
+        const url = this.getPaths("exactPath") + this.state.exactPath
         const response = await axios.get(url);
         this.setState({
             result: response.data,
@@ -25,7 +25,7 @@ class App extends Component {
     }
 
     onClickShortestPath = async () => {
-        const url = this.getPaths("shortest")+this.state.source+"/"+this.state.target
+        const url = this.getPaths("shortest") + this.state.source + "/" + this.state.target
         const data = (await axios.get(url)).data;
         this.setState({
             result: data,
@@ -34,12 +34,8 @@ class App extends Component {
     }
 
     onClickConditionOnStops = async () => {
-        let url = ""
-        if(this.state.condition === "Max"){
-            url = this.getPaths("maxNumberStops")+"Max/"+this.state.source+"/"+this.state.source+"/"+this.state.number
-        } else {
-            url = this.getPaths("exactNumberStops")+this.state.source+"/"+this.state.target+"/"+this.state.number
-        }
+        const url = this.getPaths("numberStops") + this.state.condition + "/" + this.state.source + "/" + this.state.target + "/" + this.state.number
+        console.log(url)
         const data = (await axios.get(url)).data;
         this.setState({
             result: data
@@ -47,7 +43,7 @@ class App extends Component {
     }
 
     onClickMaxDistance = async () => {
-        const url = this.getPaths("maxDistance")+this.state.source+"/"+this.state.target+"/"+this.state.max
+        const url = this.getPaths("maxDistance") + this.state.source + "/" + this.state.target + "/" + this.state.max
         const data = (await axios.get(url)).data;
         this.setState({
             result: data
@@ -57,10 +53,12 @@ class App extends Component {
     getDistanceByPath = async () => {
         const path = this.state.result
         let formattedPath = "";
-        const list = path.slice(1, path.length-1).split(", ")
-        list.forEach((stop) => {formattedPath += stop})
-        const url = this.getPaths("exactPath")+formattedPath
-        const distance =  (await axios.get(url)).data
+        const list = path.slice(1, path.length - 1).split(", ")
+        list.forEach((stop) => {
+            formattedPath += stop
+        })
+        const url = this.getPaths("exactPath") + formattedPath
+        const distance = (await axios.get(url)).data
         this.setState({
             distance: distance
         })
@@ -71,11 +69,11 @@ class App extends Component {
     }
 
     handleSourceChange(event) {
-        this.setState({source: event.target.value });
+        this.setState({source: event.target.value});
     }
 
     handleTargetChange(event) {
-        this.setState({target: event.target.value });
+        this.setState({target: event.target.value});
     }
 
     handleConditionChange(event) {
@@ -92,18 +90,15 @@ class App extends Component {
 
     getPaths(path) {
         const baseUrl = "http://localhost:8080/shortestPath/"
-        const exactNumberStops = baseUrl+"exactNumberStops/"
-        const maxNumberStops = baseUrl+"stops/"
-        const exactPath = baseUrl+"exactPath/"
-        const maxDistance = baseUrl+"maxDistance/"
-
+        const numberStops = baseUrl + "stops/"
+        const exactPath = baseUrl + "exactPath/"
+        const maxDistance = baseUrl + "maxDistance/"
+        console.log("")
         switch (path) {
             case "shortest":
                 return baseUrl
-            case "exactNumberStops":
-                return exactNumberStops
-            case "maxNumberStops":
-                return maxNumberStops
+            case "numberStops":
+                return numberStops
             case "exactPath":
                 return exactPath
             case "maxDistance":
@@ -119,78 +114,69 @@ class App extends Component {
                 <header className="App-header">
                     <h2>Welcome to ThoughtWorks Train Manage System!</h2>
                 </header>
-                <main>
-                    <div className="App-grid-container">
-                        <div className="App-exact-path">
-                            <h4>Get Distance By Exact Path</h4>
-                            <input type="text" placeholder="input like ABC" value={this.state.value} onChange={this.handleChange}/>
-                            <button onClick={this.onClickExactPath}>Execute</button>
-                        </div>
-                        <div className="App-condition-stops">
-                            <h4>Get Number of Trips With Condition on Stops</h4>
-                            <select value={this.state.condition} onChange={this.handleConditionChange.bind(this)}>
-                                <option value="" disabled selected>Select Condition</option>
-                                <option value="Max">Max</option>
-                                <option value="Equal">Equal</option>
-                            </select>
-                            {this.state.condition ?
-                                this.state.condition==="Max" ?
-                                    <>
-                                        <select id="source" value={this.state.source} onChange={this.handleSourceChange.bind(this)}>
-                                            <option value="fake" disabled selected>Select Start City</option>
-                                            {this.state.nodes.map((node) => <option value={node}>{node}</option>)}
-                                        </select>
-                                        <input value={this.state.number} placeholder="input number of stops" onChange={this.handleNumberChange.bind(this)}/>
-                                        <button onClick={this.onClickConditionOnStops}>Execute</button>
-                                    </>
-                                    :
-                                    <>
-                                        <select value={this.state.source} onChange={this.handleSourceChange.bind(this)}>
-                                            <option value="" disabled selected>Select Start City</option>
-                                            {this.state.nodes.map((node) => <option value={node}>{node}</option>)}
-                                        </select>
-                                        <select value={this.state.target} onChange={this.handleTargetChange.bind(this)}>
-                                            <option value="" disabled selected>Select End City</option>
-                                            {this.state.nodes.map((node) => <option value={node}>{node}</option>)}
-                                        </select>
-                                        <input value={this.state.number} placeholder="input number of stops" onChange={this.handleNumberChange.bind(this)}/>
-                                        <button onClick={this.onClickConditionOnStops}>Execute</button>
-                                    </>
-                                : null
-                            }
-                        </div>
-                        <div className="App-condition-distance">
-                            <h4>Get Paths with Maximum Distance</h4>
-                            <select value={this.state.source} onChange={this.handleSourceChange.bind(this)}>
-                                <option value="" disabled selected>Select Start City</option>
-                                {this.state.nodes.map((node) => <option value={node}>{node}</option>)}
-                            </select>
-                            <select value={this.state.target} onChange={this.handleTargetChange.bind(this)}>
-                                <option value="" disabled selected>Select End City</option>
-                                {this.state.nodes.map((node) => <option value={node}>{node}</option>)}
-                            </select>
-                            <input value={this.state.max} placeholder="input max distance" onChange={this.handleMaxDistanceChange.bind(this)}/>
-                            <button onClick={this.onClickMaxDistance}>Execute</button>
-                        </div>
-                        <div className="App-shortest-path">
-                            <h4>Get Shortest Path And Distance</h4>
-                            <select id="source" value={this.state.source} onChange={this.handleSourceChange.bind(this)}>
-                                <option value="" disabled selected>Select Start City</option>
-                                {this.state.nodes.map((node) => <option value={node}>{node}</option>)}
-                            </select>
-                            <select id="target" value={this.state.target} onChange={this.handleTargetChange.bind(this)}>
-                                <option value="" disabled selected>Select End City</option>
-                                {this.state.nodes.map((node) => <option value={node}>{node}</option>)}
-                            </select>
-                            <button onClick={this.onClickShortestPath}>Get Path</button>
-                            <button onClick={this.getDistanceByPath}>Get Distance</button>
-                        </div>
-                        <div className="App-result">
-                            <p>{this.state.result}</p>
-                            {this.state.distance ? <p>Distance: {this.state.distance}</p> : null}
-                        </div>
+                <div className="App-grid-container">
+                    <div className="App-exact-path">
+                        <h4>Get Distance By Exact Path</h4>
+                        <input type="text" placeholder="input like ABC" value={this.state.value}
+                               onChange={this.handleChange}/>
+                        <button onClick={this.onClickExactPath}>Execute</button>
                     </div>
-                </main>
+                    <div className="App-condition-stops">
+                        <h4>Get Number of Trips With Condition on Stops</h4>
+                        <select value={this.state.condition} onChange={this.handleConditionChange.bind(this)}>
+                            <option value="" disabled selected>Select Condition</option>
+                            <option value="Max">Max</option>
+                            <option value="Equal">Equal</option>
+                        </select>
+                        {this.state.condition ?
+                            <>
+                                <select value={this.state.source} onChange={this.handleSourceChange.bind(this)}>
+                                    <option value="" disabled selected>Select Start City</option>
+                                    {this.state.nodes.map((node) => <option value={node}>{node}</option>)}
+                                </select>
+                                <select value={this.state.target} onChange={this.handleTargetChange.bind(this)}>
+                                    <option value="" disabled selected>Select End City</option>
+                                    {this.state.nodes.map((node) => <option value={node}>{node}</option>)}
+                                </select>
+                                <input value={this.state.number} placeholder="input number of stops"
+                                       onChange={this.handleNumberChange.bind(this)}/>
+                                <button onClick={this.onClickConditionOnStops}>Execute</button>
+                            </>
+                            : null
+                        }
+                    </div>
+                    <div className="App-condition-distance">
+                        <h4>Get Paths with Maximum Distance</h4>
+                        <select value={this.state.source} onChange={this.handleSourceChange.bind(this)}>
+                            <option value="" disabled selected>Select Start City</option>
+                            {this.state.nodes.map((node) => <option value={node}>{node}</option>)}
+                        </select>
+                        <select value={this.state.target} onChange={this.handleTargetChange.bind(this)}>
+                            <option value="" disabled selected>Select End City</option>
+                            {this.state.nodes.map((node) => <option value={node}>{node}</option>)}
+                        </select>
+                        <input value={this.state.max} placeholder="input max distance"
+                               onChange={this.handleMaxDistanceChange.bind(this)}/>
+                        <button onClick={this.onClickMaxDistance}>Execute</button>
+                    </div>
+                    <div className="App-shortest-path">
+                        <h4>Get Shortest Path And Distance</h4>
+                        <select id="source" value={this.state.source} onChange={this.handleSourceChange.bind(this)}>
+                            <option value="" disabled selected>Select Start City</option>
+                            {this.state.nodes.map((node) => <option value={node}>{node}</option>)}
+                        </select>
+                        <select id="target" value={this.state.target} onChange={this.handleTargetChange.bind(this)}>
+                            <option value="" disabled selected>Select End City</option>
+                            {this.state.nodes.map((node) => <option value={node}>{node}</option>)}
+                        </select>
+                        <button onClick={this.onClickShortestPath}>Get Path</button>
+                        <button onClick={this.getDistanceByPath}>Get Distance</button>
+                    </div>
+                    <div className="App-result">
+                        <p>{this.state.result}</p>
+                        {this.state.distance ? <p>Distance: {this.state.distance}</p> : null}
+                    </div>
+                </div>
                 <footer className="App-footer">
                     All rights reserved by ThoughtWorks.
                 </footer>
